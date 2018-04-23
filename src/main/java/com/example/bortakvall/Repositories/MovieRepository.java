d8e744e048b7ea875539aaeb20a8ac82d22af213
Martina J:son Gren <martinajsongren@hotmail.se>
2018-04-23 19:58:57 +0200

    Squashed commit of the following:
    commit ec7ff36301f89c99992a7a1615cb9add69f9ba38
    Author: Frans Herrstrom <frans.herrstrom@gmail.com>
    Date:   Mon Apr 23 16:04:24 2018 +0200
    
        validation and authentication completed
    
    commit f5933f475298e1f3da10629e0c3da9b640058b94
    Author: Frans Herrstrom <frans.herrstrom@gmail.com>
    Date:   Mon Apr 23 11:22:56 2018 +0200
    
        css och rentedindex linkar fixad

--- src/main/java/com/example/bortakvall/Repositories/MovieRepository.java
+++ src/main/java/com/example/bortakvall/Repositories/MovieRepository.java
@@ -1,13 +1,19 @@
 package com.example.bortakvall.Repositories;
 
 import com.example.bortakvall.entity.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface MovieRepository extends JpaRepository<Movie, String> {
 
     List<Movie> findMovieByCustomerSocialsecuritynumber(String socialsecuritynumber);
     List<Movie> findAllByCustomerSocialsecuritynumber(String socialsecuritynumber);
 
+    Page<Movie> findAllByNameContainingIgnoreCase(String name, Pageable pageable);
 }
